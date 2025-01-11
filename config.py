2392b30102f8c04854f0042263adc10612193859
MUHAMMAD ABDULLAH <printabdullah285@gmail.com>
2025-01-11 12:56:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from pymongo import MongoClient
 
 class Config:
     SECRET_KEY = 'supersecretkey'
-    MONGO_URI = 'mongodb+srv://newtesting:dbfortesting285@cluster0.crh19.mongodb.net/' 
+    MONGO_URI = 'mongodb+srv://<db_username>:<db_password>@cluster0.crh19.mongodb.net/' 
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
